0d790892a1d402c9416d0cdb361c0412fd96ba5a
aldosuwandi <aldosuwandi@gmail.com>
2015-05-27 12:17:10 +0700

    update database

--- app/Http/Controllers/Admin/ConfigController.php
+++ app/Http/Controllers/Admin/ConfigController.php
@@ -15,10 +15,56 @@ class ConfigController extends AdminController {
         return view('admin.config');
     }
 
+    public function anyDatabase($operation)
+    {
+        $username = \Config::get('database.connections.mysql.username');
+        $password = \Config::get('database.connections.mysql.password');
+        $database = \Config::get('database.connections.mysql.database');
+
+        if ($operation == 'dump') {
+            exec('mysqldump  --opt --user='.$username.' --password='.$password.' --host=localhost '.$database.' > ../neuro.sql');
+            $file= public_path(). "/../neuro.sql";
+            return \Response::download($file, 'neuro.sql');
+        } else if ($operation == 'import') {
+            \Request::file('sql_file')->move('temp','temp.sql');
+            exec('mysql --user='.$username.' --password='.$password.'  -e "create database if not exists '.$database.'"');
+            exec('mysql --user='.$username.' --password='.$password.'  -e "drop database neuroscience" '.$database);
+            exec('mysql --user='.$username.' --password='.$password.'  -e "create database if not exists '.$database.'"');
+            exec('mysql --user='.$username.' --password='.$password.' '.$database.' < temp/temp.sql');
+            unlink('temp/temp.sql');
+            \Flash::info('Database imported !');
+            return redirect('admin/config');
+        }
+    }
+
     public function getUpdate()
     {
-        $result =  exec('git pull --rebase');
+        $result = false;
+        \Artisan::call('down');
+        exec('git pull --rebase');
+        exec('composer update -d=../');
+        \Artisan::call('up');
         return $result;
     }
+
+    public function getCache($operation)
+    {
+        if ($operation == 'clear') {
+            \Artisan::call('cache:clear');
+            \Flash::info('App cache cleared !');
+            return redirect('admin/config');
+        } else if ($operation == 'route') {
+            \Artisan::call('route:cache');
+            return 'Route cached !';
+        } else if ($operation == 'config') {
+            \Artisan::call('config:clear');
+            \Artisan::call('config:cache');
+            return 'Config cached updated !';
+        }
+    }
+
+
+
+
 
 }
--- resources/views/admin/config.blade.php
+++ resources/views/admin/config.blade.php
@@ -2,14 +2,32 @@
 
 @section('content')
     <h4> Version 0.5-Beta </h4>
+    <a class="btn  btn-success" href="/admin/config/update">Update</a>
     <hr/>
 
-    <h4> Database </h4>
+    <h4>Dump Database </h4>
+    <a class="btn  btn-danger" href="/admin/config/database/dump">Dump</a>
+    <hr/>
+
+    <h4>Import Database </h4>
+    {!!Form::open([
+        'url'=>'/admin/config/database/import',
+        'method'=>'POST',
+        'files'=> true
+    ])!!}
+    <div class="control-group form-group">
+        <div class="controls">
+            {!!Form::file('sql_file') !!}
+        </div>
+        <br/>
+        <button class="btn btn-info" type="submit">Import</button>
+    </div>
+    {!!Form::close()!!}
     <hr/>
 
     <h4> Cache </h4>
-    <hr/>
-
-    <h4> Environment </h4>
+    <a class="btn  btn-success" href="/admin/config/cache/clear">Clear App Cache</a>
+    <a class="btn  btn-success" href="/admin/config/cache/route">Cache Route</a>
+    <a class="btn  btn-success" href="/admin/config/cache/config">Cache Config</a>
     <hr/>
 @stop
